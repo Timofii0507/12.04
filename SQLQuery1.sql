f9f0142459d627d9167e5cd6f6f5f7ca3273da0d
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-05-03 15:34:14 +0300

    Update SQLQuery1.sql
    select  дз1

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+USE [Academy];
+
+-- Вибірка даних з таблиці "Groups"
+SELECT * FROM Groups;
+
+-- Вибірка даних з таблиці "Faculties"
+SELECT * FROM Faculties;
+
+-- Вибірка даних з таблиці "Teachers"
+SELECT * FROM Teachers;
