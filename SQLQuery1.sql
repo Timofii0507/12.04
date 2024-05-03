a581fccb4fcaaee39479a14fd8fb8c49162935d6
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-05-03 15:20:48 +0300

    Update SQLQuery1.sql
    сreate дз1

---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE [Academy]
+GO
+USE [Academy]
+GO
+
+CREATE TABLE Groups (
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE Faculties (
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Teachers (
+    Id INT PRIMARY KEY IDENTITY,
+    EmploymentDate DATE NOT NULL CHECK (EmploymentDate >= '1990-01-01'),
+    Name NVARCHAR(MAX) NOT NULL,
+    Premium MONEY NOT NULL CHECK (Premium >= 0) DEFAULT 0,
+    Salary MONEY NOT NULL CHECK (Salary > 0),
+    Surname NVARCHAR(MAX) NOT NULL
+);
